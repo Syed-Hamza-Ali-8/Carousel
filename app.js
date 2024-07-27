4e2262fef816a38eee79a0dc0ff0ab1bf1de0323
Syed-Hamza-Ali-8 <ha5755420@gmail.com>
2024-07-26 22:52:16 -0700

    carosuel

---
+++ app.js
@@ -0,0 +1,45 @@
+let cricketersCarosule = [
+    "BA56.jpg",
+    "SS49.jpg",
+    "KW22.jpg",
+    "JB63.jpg",
+];
+
+const cricketers = ["BABAR AZAM", "STEVE SMITH", "KANE WILLIAMSON", "JOS BUTTLER"]
+
+let currentIndex = 0;
+
+let image = document.getElementById("slider-image");
+
+let cricketersName = document.getElementById("BA")
+
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+
+function update() {
+    image.src = cricketersCarosule[currentIndex];
+    cricketersName.innerText = cricketers[currentIndex]
+}
+
+next.addEventListener("click", () => {
+    currentIndex++;
+    if (currentIndex >= cricketersCarosule.length) {
+        currentIndex = 0
+    }
+
+    update()
+
+});
+
+prev.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = cricketersCarosule.length - 1;
+
+    }
+
+    update()
+
+});
+
+update()
